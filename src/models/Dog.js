53b5e750f42fd3d97008dc74e530069a6365a334
Ignacio Cunial <igsecu@hotmail.com>
2023-08-15 15:50:24 -0300

    Created models folder / Created Dog.js model

---
+++ src/models/Dog.js
@@ -0,0 +1,64 @@
+const { DataTypes } = require("sequelize");
+const db = require("../db");
+
+const Dog = db.define(
+  "dog",
+  {
+    id: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    min_height: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    max_height: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    height: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    min_weight: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    max_weight: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    weight: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    min_life_span: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    max_life_span: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    life_span: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    image: {
+      type: DataTypes.STRING,
+    },
+  },
+  {
+    timestamp: true,
+    createdAt: true,
+    updatedAt: false,
+  }
+);
+
+module.exports = Gig;

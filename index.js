7a40b51f21f9aa0964003943e33c65ae1695df2f
Ignacio Cunial <igsecu@hotmail.com>
2023-08-15 15:40:59 -0300

    Installed dotenv / Synchronized database -> index.js

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const app = express();
+const PORT = process.env.PORT || 5000;
+const db = require("./src/db");
 
 // Body-Parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // Initialized Express Server
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}...`);
+db.sync({ force: true }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}...`);
+  });
 });

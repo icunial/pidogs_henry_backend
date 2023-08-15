2bf744dc0e4b64dfc7f0cb94d39601d1da2c81b1
Ignacio Cunial <igsecu@hotmail.com>
2023-08-15 17:43:47 -0300

    Created routes folder / dog.js route

---
+++ src/routes/dogs.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+
+const Dog = require("../models/Dog");
+const Temperament = require("../models/Temperament");
+
+module.exports = router;

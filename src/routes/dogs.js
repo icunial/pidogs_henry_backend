22959e934ef4577b01c5d982914449eb1290446f
Ignacio Cunial <igsecu@hotmail.com>
2023-08-16 22:30:23 -0300

    Created search dogs by its name feature -> GET / route

--- src/routes/dogs.js
+++ src/routes/dogs.js
@@ -13,6 +13,11 @@ router.get("/:id", async (req, res, next) => {
 
   try {
     result = await dogController.findDogByIdApi(id);
+    if (!result.length)
+      return res.status(404).json({
+        statusCode: 404,
+        msg: `Dog with ID: ${id} not found!`,
+      });
     res.status(200).json({
       statusCode: 200,
       data: result,
@@ -24,7 +29,22 @@ router.get("/:id", async (req, res, next) => {
 
 // GET all dogs
 router.get("/", async (req, res, next) => {
+  const { name } = req.query;
+
   try {
+    // Search dogs by its name
+    if (name) {
+      const apiResults = await dogController.findByNameApi(name);
+      if (!apiResults.length)
+        return res.status(404).json({
+          statusCode: 404,
+          msg: `Dog with name ${name} not found!`,
+        });
+      return res.status(200).json({
+        statusCode: 200,
+        data: apiResults,
+      });
+    }
     const apiResults = await dogController.getAllApi();
     return res.status(200).json({
       statusCode: 200,

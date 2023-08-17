5570325b6dce02a4e6f5b2644e041f7468eae9fd
Ignacio Cunial <igsecu@hotmail.com>
2023-08-16 22:15:22 -0300

    Created findDogByIdApi() controller -> src/controllers/dogs.js

--- src/controllers/dogs.js
+++ src/controllers/dogs.js
@@ -24,6 +24,34 @@ const getAllApi = async () => {
   }
 };
 
+// Get dog by its id from API
+const findDogByIdApi = async (id) => {
+  const result = [];
+
+  try {
+    const apiResults = await axios.get("https://api.thedogapi.com/v1/breeds");
+    if (apiResults) {
+      apiResults.data.forEach((r) => {
+        if (r.id === parseInt(id)) {
+          result.push({
+            id: r.id,
+            name: r.name,
+            image: r.reference_image_id,
+            temperament: r.temperament,
+            weight: r.weight.metric,
+            height: r.height.metric,
+            life_span: r.life_span,
+          });
+        }
+      });
+    }
+    return result;
+  } catch (error) {
+    throw new Error("Error finding a dog by its ID from API");
+  }
+};
+
 module.exports = {
   getAllApi,
+  findDogByIdApi,
 };

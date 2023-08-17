7c864a632e3885595cefe6cbf41782cf03ae7d84
Ignacio Cunial <igsecu@hotmail.com>
2023-08-16 22:10:37 -0300

    Created GET /:id -> get dog by its id -> dogRouter

--- src/routes/dogs.js
+++ src/routes/dogs.js
@@ -6,6 +6,22 @@ const Temperament = require("../models/Temperament");
 
 const dogController = require("../controllers/dogs");
 
+// Get dog by its id
+router.get("/:id", async (req, res, next) => {
+  const id = req.params.id;
+  let result = [];
+
+  try {
+    result = await dogController.findDogByIdApi(id);
+    res.status(200).json({
+      statusCode: 200,
+      data: result,
+    });
+  } catch (error) {
+    return next(error);
+  }
+});
+
 // GET all dogs
 router.get("/", async (req, res, next) => {
   try {

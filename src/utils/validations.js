0d63e9dbb563f97bf3d546922129b4b3e6b2388d
Ignacio Cunial <igsecu@hotmail.com>
2023-08-17 22:31:01 -0300

    Created validateWeight() function -> validates dogs weight

--- src/utils/validations.js
+++ src/utils/validations.js
@@ -1,4 +1,4 @@
-// Validate name input -> if it is missing, if it is a string or not and its length
+// Validates name input -> if it is missing, if it is a string or not and its length
 const validateName = (name) => {
   if (!name) return "Name parameter is missing";
   if (typeof name !== "string") return "Name must be a string!";
@@ -7,7 +7,7 @@ const validateName = (name) => {
   return false;
 };
 
-// Validate height input property
+// Validates height input property
 const validateHeight = (min_height, max_height) => {
   if (!min_height) return "Min Height parameter is missing!";
   if (!max_height) return "Max Height parameter is missing!";
@@ -26,6 +26,26 @@ const validateHeight = (min_height, max_height) => {
   return false;
 };
 
+// Validates weight input property
+const validateWeight = (min_weight, max_weight) => {
+  if (!min_weight) return "Min Weight parameter is missing!";
+  if (!max_weight) return "Max Weight parameter is missing!";
+  if (typeof min_weight !== "number") return "Min Weight must be a number!";
+  if (typeof max_weight !== "number") return "Max Weight must be a number!";
+  if (
+    min_weight > 100 ||
+    min_weight < 1 ||
+    max_weight > 100 ||
+    max_height < 1
+  ) {
+    return "Weight must be between 1 and 100";
+  }
+  if (max_weight < min_weight)
+    return "Max Weight must be higher than Min Weight";
+  return false;
+};
+
 module.exports = {
   validateName,
+  validateHeight,
 };

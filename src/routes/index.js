10fb632216c36b53e0e5e48ccf4ebbdc6aa96eba
Ignacio Cunial <igsecu@hotmail.com>
2023-08-19 16:34:35 -0300

    Created temperaments router -> /src/routes/temperaments / Imported and used -> /src/routes/index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const router = express.Router();
 
 const dogsRouter = require("./dogs");
+const temperamentsRouter = require("./temperaments");
 
 // Specify router root route
 router.use("/dogs", dogsRouter);
+router.use("/temperaments", temperamentsRouter);
 
 module.exports = router;
---
+++ src/routes/temperaments.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+
+const Dog = require("../models/Dog");
+const Temperament = require("../models/Temperament");
+
+const axios = require("axios");
+
+module.exports = router;

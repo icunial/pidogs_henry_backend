ff5eaf1475838557f165893057da551a8d9a007a
Ignacio Cunial <igsecu@hotmail.com>
2023-08-19 16:56:38 -0300

    Created GET / route -> gets all temperaments from api -> all not repeated from all dogs

--- src/routes/temperaments.js
+++ src/routes/temperaments.js
@@ -6,4 +6,31 @@ const Temperament = require("../models/Temperament");
 
 const axios = require("axios");
 
+// GET all temperaments
+router.get("/", async (req, res, next) => {
+  try {
+    let response = await axios.get("https://api.thedogapi.com/v1/breeds");
+
+    let temperamentsFromApi = [];
+
+    response = response.data.map((item) => {
+      return item.temperament;
+    });
+
+    response.forEach((item) => {
+      if (item !== undefined) {
+        item.split(", ").forEach((i) => {
+          temperamentsFromApi.push(i);
+        });
+      }
+    });
+
+    temperamentsFromApi = Array.from(new Set(temperamentsFromApi));
+
+    console.log(temperamentsFromApi);
+  } catch (error) {
+    return next("Error trying to get all temperaments");
+  }
+});
+
 module.exports = router;

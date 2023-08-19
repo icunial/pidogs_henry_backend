d6e3ab580f3ae87d109183d611f6133a0bf03563
Ignacio Cunial <igsecu@hotmail.com>
2023-08-19 17:27:30 -0300

    Created Res Headers -> index.js

--- index.js
+++ index.js
@@ -9,6 +9,18 @@ const router = require("./src/routes/index");
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+// Res Headers
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
+
 // Router middleware
 app.use("/", router);
 
